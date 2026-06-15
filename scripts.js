abe1ab25999e4d7fe7e4debe628ba4ad020060c6
ElitoniCR1985 <elitonirosacarvalho@hotmail.com>
2026-06-15 20:06:06 -0300

    Formatação do codigo

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ let list = document.querySelectorAll('.item')
 let next = document.getElementById('next')
 let prev = document.getElementById('prev')
 
-let count = list.length 
+let count = list.length
 let active = 0
 
 console.log(list)
@@ -11,7 +11,7 @@ next.onclick = () => {
     let activeOld = document.querySelector('.active')
     activeOld.classList.remove('active')
 
-    active = active >= count -1 ? 0 : active + 1
+    active = active >= count - 1 ? 0 : active + 1
     list[active].classList.add('active')
 }
 
@@ -19,10 +19,10 @@ prev.onclick = () => {
     let activeOld = document.querySelector('.active')
     activeOld.classList.remove('active')
 
-     active = active <= 0 ? count -1 : active - 1
+    active = active <= 0 ? count - 1 : active - 1
     list[active].classList.add('active')
 }
 
-    
+
 
 
