a211f2192c72cd7254451be824fccca96c0ea4e1
ElitoniCR1985 <elitonirosacarvalho@hotmail.com>
2026-08-07 08:47:15 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,30 @@
-let list = document.querySelectorAll('.item')
-let next = document.getElementById('next')
-let prev = document.getElementById('prev')
 
-let count = list.length
-let active = 0
+let list = document.querySelectorAll(".item");
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
 
-console.log(list)
+let count = list.length;
+let active = 0;
+
+console.log(list);
 
 next.onclick = () => {
-    let activeOld = document.querySelector('.active')
-    activeOld.classList.remove('active')
+    let activeOld = document.querySelector(".item.active");
+    activeOld.classList.remove("active");
 
-    active = active >= count - 1 ? 0 : active + 1
-    list[active].classList.add('active')
-}
+    active = active >= count - 1 ? 0 : active + 1;
+
+    list[active].classList.add("active");
+};
 
 prev.onclick = () => {
-    let activeOld = document.querySelector('.active')
-    activeOld.classList.remove('active')
+    let activeOld = document.querySelector(".item.active");
+    activeOld.classList.remove("active");
 
-    active = active <= 0 ? count - 1 : active - 1
-    list[active].classList.add('active')
-}
+    active = active <= 0 ? count - 1 : active - 1;
+
+    list[active].classList.add("active");
+};
 
 
 
